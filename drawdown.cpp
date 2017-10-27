23308580a6222505b818a410b6e49ca9295b4304
Patrick O'Brien <obrienpja@gmail.com>
2017-10-26 23:34:45 -0400

    Creates random equity curve

--- drawdown.cpp
+++ drawdown.cpp
@@ -18,19 +18,25 @@ int main()
   srand (time(NULL));
   std::vector<double> indices;
 
-  int N;
+  int N = 4;
 
-  std::cout << "Enter the number of days:" << std::endl;
-  std::cin >> N;
+  // std::cout << "Enter the number of days:" << std::endl;
+  // std::cin >> N;
   std::vector<double> returnProfile = {1.5, -.5, -.5, -.5};
   std::vector<double> result(4);
 
+  std::cout << "The equity curve is: " << std::endl;
+  std::cout << "--------------------------" << std::endl;
+
+  double total = 0;
+
   for(int i = 0; i < N; i++)
   {
-    indices.push_back(*(returnProfile.begin() + randFunc(4)));
+    total += *(returnProfile.begin() + randFunc(returnProfile.size()));
+    indices.push_back(total);
   }
 
-  std::partial_sum(returnProfile.begin(), returnProfile.end(),  std::ostream_iterator<double>(std::cout, " "));
+  // std::partial_sum(returnProfile.begin(), returnProfile.end(),  std::ostream_iterator<double>(std::cout, " "));
 
   for(std::vector<double>::iterator i = indices.begin() ; i != indices.end(); i++)
   {
@@ -43,12 +49,12 @@ int main()
   // }
 
 
-  std::cout << "The value of the thing is: " << *(returnProfile.begin()+1) << std::endl;
+  // std::cout << "The value of the thing is: " << *(returnProfile.begin()+1) << std::endl;
 
-  for(std::vector<double>::iterator i = returnProfile.begin() ; i != returnProfile.end(); i++)
-  {
-    std::cout << *i << std::endl;
-  }
+  // for(std::vector<double>::iterator i = returnProfile.begin() ; i != returnProfile.end(); i++)
+  // {
+  //   std::cout << *i << std::endl;
+  // }
 
   return 0;
 }
